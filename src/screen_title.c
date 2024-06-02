f9786f94bf94b7c7a04c4bacadcd098e9e3630a7
liam-earle <38230039+liam-earle@users.noreply.github.com>
2024-06-02 13:41:22 -0230

    code clean up

--- src/screen_title.c
+++ src/screen_title.c
@@ -3,101 +3,19 @@
 #include "screens.h"
 #include <stdio.h> 
 #include <math.h>
+#include "gauges.h"
 
 static int framesCounter = 0;
 static int finishScreen = 0;
 static float heightOffset = 30.0f;
-int rpm = 0;
-float rpmMax = 4000.0f;
-int speed = 0;
-float speedMax = 75.0f;
+int rpm = 3000;
+const int rpmMax = 5000;
+int speed = 15;
+const int speedMax = 50;
 float temp = 0;
 float voltage = 6;
 float angle = 225;
 bool maxAngle = false;
-
-void calculateSpeedLocations(double centerX, double centerY, double radius, double startAngle, double endAngle) {
-	// Convert angles to radians
-	startAngle = startAngle * M_PI / 180.0;
-	endAngle = endAngle * M_PI / 180.0;
-
-    int initSpeed = 0;
-
-	// Calculate the number of steps
-	int numSteps = 10;
-
-	// Calculate the angle increment per step
-	double angleIncrement = (endAngle - startAngle) / numSteps;
-
-	// Calculate and print the coordinates
-	for (int i = 0; i <= numSteps; i++) {
-		double angle = startAngle + i * angleIncrement;
-		double x = centerX + radius * cos(angle);
-		double y = centerY + radius * sin(angle);
-
-		DrawTextEx(
-            font,
-            TextFormat("%i", initSpeed),
-            (Vector2){ x-(MeasureTextEx(font, TextFormat("%i", initSpeed), 24, 2).x)/2.0f, y },
-            24,
-            2,
-            RAYWHITE
-        );
-        initSpeed += 5;
-	}
-}
-
-void calculateRPMLocations(double centerX, double centerY, double radius, double startAngle, double endAngle) {
-	// Convert angles to radians
-	startAngle = startAngle * M_PI / 180.0;
-	endAngle = endAngle * M_PI / 180.0;
-
-    int initRPM = 0;
-
-	// Calculate the number of steps
-	int numSteps = 5;
-
-	// Calculate the angle increment per step
-	double angleIncrement = (endAngle - startAngle) / numSteps;
-
-	// Calculate and print the coordinates
-	for (int i = 0; i <= numSteps; i++) {
-		double angle = startAngle + i * angleIncrement;
-		double x = centerX + radius * cos(angle);
-		double y = centerY + radius * sin(angle);
-
-		DrawTextEx(
-            font,
-            TextFormat("%i", initRPM),
-            (Vector2){ x-(MeasureTextEx(font, TextFormat("%i", initRPM), 24, 2).x)/2.0f, y },
-            24,
-            2,
-            RAYWHITE
-        );
-        initRPM += 1;
-	}
-}
-
-void DrawCircularGauge(Vector2 gaugeCenter) {
-    float innerRadius = 195.0f;
-    float outerRadius = 200.0f;
-    float startAngle = -40.0f;
-    float endAngle = -320.0f;
-    int segments = 180;
-
-    DrawRing(gaugeCenter, innerRadius, outerRadius, startAngle, endAngle, segments, SKYBLUE);
-    DrawRing(gaugeCenter, innerRadius-10, outerRadius-10, startAngle, endAngle, segments, WHITE);
-    DrawRing(gaugeCenter, innerRadius-100, outerRadius-100, startAngle+22.5, endAngle-22.5, segments, SKYBLUE);
-}
-
-void DrawNeedle(Vector2 gaugeCenter) {
-    rlPushMatrix();
-        rlTranslatef(gaugeCenter.x, gaugeCenter.y, 0);
-        rlRotatef(angle, 0, 0, -1);
-        DrawRectangle(0, 0, 165, 5, RED);
-    rlPopMatrix();
-    DrawCircle(gaugeCenter.x, gaugeCenter.y, 95, BLACK);
-}
 
 // Title Screen Initialization logic
 void InitTitleScreen(void) {
@@ -116,14 +34,15 @@ void DrawTitleScreen(void) {
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK);
 
     // LEFT HAND SIDE ( RPM )
-    Vector2 rpmCenter = { (GetScreenWidth() / 4.0f), (GetScreenHeight() / 2.0f) + heightOffset };
-    Vector2 speedCenter = { (GetScreenWidth() / 1.35f), (GetScreenHeight() / 2.0f) + heightOffset };
+    Vector2 speedCenter = { (GetScreenWidth() / 4.0f), (GetScreenHeight() / 2.0f) + heightOffset };
+    Vector2 rpmCenter = { (GetScreenWidth() / 1.35f), (GetScreenHeight() / 2.0f) + heightOffset };
 
-    DrawRectangle(rpmCenter.x, rpmCenter.y-160, speedCenter.x-rpmCenter.x, 320, (Color){102, 191, 255, 155});
+    DrawRectangle(speedCenter.x, speedCenter.y-160, rpmCenter.x-speedCenter.x, 320, (Color){102, 191, 255, 155});
 
-    DrawCircle(rpmCenter.x, rpmCenter.y, 210, BLACK);
-    DrawNeedle(rpmCenter);
-    DrawCircularGauge(rpmCenter);
+    DrawCircle(speedCenter.x, speedCenter.y, 210, BLACK);
+    DrawNeedle(speedCenter, speed, speedMax);
+    DrawCircularGauge(speedCenter);
+    DrawSemiCircularGauge(speedCenter, true);
 
     DrawTextEx(
         font,
@@ -141,12 +60,13 @@ void DrawTitleScreen(void) {
         2,
         RAYWHITE
     );
-    calculateSpeedLocations(rpmCenter.x, rpmCenter.y-10, 160, -225.0f, 45.0f);
+    calculateSpeedLocations(speedCenter.x, speedCenter.y-10, 160, -225.0f, 45.0f, &font);
     // calculateSemiCircle(rpmCenter.x, rpmCenter.y-10, 150, 360, 0, 10);
 
-    DrawCircle(speedCenter.x, speedCenter.y, 210, BLACK);
-    DrawNeedle(speedCenter);
-    DrawCircularGauge(speedCenter);
+    DrawCircle(rpmCenter.x, rpmCenter.y, 210, BLACK);
+    DrawNeedle(rpmCenter, rpm, rpmMax);
+    DrawCircularGauge(rpmCenter);
+    DrawSemiCircularGauge(rpmCenter, false);
 
     DrawTextEx(
         font,
@@ -156,6 +76,7 @@ void DrawTitleScreen(void) {
         2,
         RAYWHITE
     );
+    
     DrawTextEx(
         font,
         TextFormat("%i", rpm),
@@ -165,19 +86,18 @@ void DrawTitleScreen(void) {
         RAYWHITE
     );
 
-    calculateRPMLocations(speedCenter.x, speedCenter.y-10, 160, -225.0f, 45.0f);
+    calculateRPMLocations(rpmCenter.x, rpmCenter.y-10, 160, -225.0f, 45.0f, &font);
 
-    // Gauge Sweep
-    if (angle >= -40.0f && maxAngle == false) {
-        // printf("%f \n", angle);
-        angle -= 3.5;
-    } else {
+    if (speed == speedMax && !maxAngle) {
         maxAngle = true;
+    } else if (speed <= 0 && maxAngle) {
+        speed = 0;
+    } else if (speed <= speedMax && maxAngle) {
+        speed -= 1;
+    } else if(!maxAngle) {
+        speed += 1;
     }
-    if (angle <= 225 && maxAngle == true) {
-        angle += 3.5;
-    } 
-    DrawFPS(10, 10);
+    DrawFPS(1, 1);
 }
 
 // Title Screen Unload logic
